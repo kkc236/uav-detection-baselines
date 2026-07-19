49a2d12148be01dc78438d0b4c89c93f0e2cb621
kkc236 <3540644328@qq.com>
2026-07-19 15:36:35 +0800

    Make checkpoint sync safe for concurrent servers

--- scripts/run_vsf_rmr_server.sh
+++ scripts/run_vsf_rmr_server.sh
@@ -86,7 +86,7 @@ sync_arguments=(
   --tag "$TAG"
   --source-branch "$SOURCE_BRANCH"
   --status-file "$SYNC_STATUS"
-  --retain 3
+  --retain 1
   --asset-prefix "$ASSET_PREFIX"
   --release-name "$RELEASE_NAME"
   --release-body "$RELEASE_BODY"
